bb430102150db99c585e224702f3217ce37ca0bc
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-02-25 17:59:50 -0500

    adding myhtml template

--- index.js
+++ index.js
@@ -1,74 +1,106 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-const questions = [
-    {
-        type: 'input',
-        message: "The employees email:",
-        name: 'email',
-    },
-    {
-        type: 'input',
-        message: "The employees github username link",
-        name: 'github',
-    },
-    {
-        type: 'input',
-        message: "Team manager's name",
-        name: 'installation',
-    },
-    {
-        type: 'input',
-        message: "Usage information:",
-        name: 'usage',
-    },
-    {
-        type: 'input',
-        message: "Contribution guidelines:",
-        name: 'contributing',
-    },
-    {
-        type: 'input',
-        message: "Test insructions:",
-        name: 'test',
-    },
-    {
-        type: 'list',
-        message: "License:",
-        name: 'license',
-        choices: [
-            "MIT",
-            "Apache",
-            "GPL",
-            "None"
-        ]
-    },
-    // {
-    //     type: 'input',
-    //     message: "Github username:",
-    //     name: 'github',
-    // },
-    // {
-    //     type: 'input',
-    //     message: "Email me at:",
-    //     name: 'email'
-    // },
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, response) { 
-fs.writeFile(fileName, response, err => err ? console.error(err) : console.log('Success!'));
-}
-
-// TODO: Create a function to initialize app
-function init() { 
 inquirer
-    .prompt(questions)
-    .then((response) => {
-        const contents = generateHtmlPage(response);
-        writeToFile("./dist/index.html", contents);
-    });
-}
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Where are you located?',
+            name: 'location',
+        },
+        {
+            type: 'input',
+            message: 'Short bio?',
+            name: 'bio',
+        },
+        {
+            type: 'input',
+            message: 'Linkedin URL?',
+            name: 'linkedin',
+        },
+        {
+            type: 'input',
+            message: 'Github URL?',
+            name: 'github',
+        },
+    ]).then((response) =>
+        fs.writeFile(
+            `./dist/index.html`,
 
-// Function call to initialize app
-init();
+            `
+            <!DOCTYPE html>
+            <html lang="en">
+            
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+                    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+                <link rel="stylesheet" href="style.css">
+                <title>Document</title>
+            </head>
+            
+            <body>
+                <div class="row">
+                    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;" text-align="center">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <h5 class="card-title">Light card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <h5 class="card-title">Light card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <h5 class="card-title">Light card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="row">
+                    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;" text-align="center">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <h5 class="card-title">Light card title</h5>
+                            <input class="form-control" type="text" placeholder="Default input" aria-label="default input example">
+                        </div>
+                    </div>
+                    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <h5 class="card-title">Light card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            
+            
+                <script src="code.js"></script>
+            </body>
+            
+            </html>
+`
+
+            ,
+            (err) => err ? console.log("There was an error!") : console.log("Successfully appended to file!")
+
+
+        )
+    );
