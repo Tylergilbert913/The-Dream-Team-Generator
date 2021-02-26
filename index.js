88ef05d3ce775bf9ae0a4b9ca7e44ae2eb251917
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-02-26 14:09:35 -0500

    recreating my html temeplate

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const jest = require("jest");
 
 inquirer
     .prompt([
@@ -34,68 +35,69 @@ inquirer
 
             `
             <!DOCTYPE html>
-            <html lang="en">
-            
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-                    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-                <link rel="stylesheet" href="style.css">
-                <title>Document</title>
-            </head>
-            
-            <body>
-                <div class="row">
-                    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;" text-align="center">
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                            <h5 class="card-title">Light card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                        </div>
-                    </div>
-                    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                            <h5 class="card-title">Light card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                        </div>
-                    </div>
-                    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                            <h5 class="card-title">Light card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                        </div>
-                    </div>
-                </div>
-            
-                <div class="row">
-                    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;" text-align="center">
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                            <h5 class="card-title">Light card title</h5>
-                            <input class="form-control" type="text" placeholder="Default input" aria-label="default input example">
-                        </div>
-                    </div>
-                    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                            <h5 class="card-title">Light card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                        </div>
-                    </div>
-                </div>
-            
-            
-                <script src="code.js"></script>
-            </body>
-            
-            </html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="row">
+        <div class="card text-dark bg-light mb-3" style="max-width: 18rem;" text-align="center">
+            <div class="card-header"></div>
+            <div class="card-header"></div>
+            <div class="card-body">
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+
+            </div>
+        </div>
+        <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+            <div class="card-header"></div>
+            <div class="card-header"></div>
+            <div class="card-body">
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+            </div>
+        </div>
+        <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+            <div class="card-header"></div>
+            <div class="card-header"></div>
+            <div class="card-body">
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="card text-dark bg-light mb-3" style="max-width: 18rem;" text-align="center">
+            <div class="card-header"></div>
+            <div class="card-header"></div>
+            <div class="card-body">
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+            </div>
+        </div>
+        <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+            <div class="card-header"></div>
+            <div class="card-header"></div>
+            <div class="card-body">
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+</body>
+
+</html>
 `
 
             ,
