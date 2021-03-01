d7d2c547ff4446f77cea24bd48fe5fe29edd9c8e
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-01 11:50:03 -0500

    added require connecting libraries with my index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const jest = require("jest");
 
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+
 function initialPrompt() {
     return inquirer.prompt([
         {
