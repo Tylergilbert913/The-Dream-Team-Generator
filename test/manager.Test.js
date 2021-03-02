181302a23007ad83f0e5a157015b1a9348da2d93
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-02 16:31:47 -0500

    addind comments to manager test

--- test/manager.Test.js
+++ test/manager.Test.js
@@ -1,6 +1,6 @@
 const { describe, it, expect } = require("@jest/globals");
 const Manager = require("../lib/manager");
-
+// manager class variables
 describe("Manager class", () => {
     const name = "Rebecca";
     const id = "456321";
@@ -8,13 +8,13 @@ describe("Manager class", () => {
     const officeNumber = 123421421423;
     const manager = new Manager(name, id, email, officeNumber);
     
-
+// ddescribes what should be returned when getOfficeNumber method is invoked
     describe('getOfficeNumber', () => {
         it("Should return manager's office number as a string", () => {
             expect(manager.getOfficeNumber()).toEqual(officeNumber);
         });
     });
-
+// decsribes what should return when getRole method is invoked
     describe('getRole', () => {
         it("Should return manager's role as as string", () => {
             expect(manager.getRole()).toEqual("Manager");
