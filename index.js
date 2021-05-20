13507b6eb9b952dda772b4c0d43b14f72e2b1f76
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-05-20 17:23:44 -0400

    removed the nredser file in lib

--- index.js
+++ index.js
@@ -24,85 +24,85 @@ async function init() {
 
     // Function that calls the initial inquirer.prompt
     await inquirer.prompt([
-            {
-                type: "list",
-                message: "Welcome to Team Profile Generator! Select the 'Manager' role to continue.",
-                name: 'role',
-                choices: ['Manager']
-            },
-            {
-                type: 'input',
-                message: "Please enter the team manager's full name.",
-                name: 'employeeName',
-                validate: (answer) => {
-                    valid = /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/.test(answer)
-                    if (!valid) {
-                        return console.log(" *Name input cannot contain any numbers or symbols. Try again.*")
-                    }
-                    return true;
+        {
+            type: "list",
+            message: "Welcome to Team Profile Generator! Select the 'Manager' role to continue.",
+            name: 'role',
+            choices: ['Manager']
+        },
+        {
+            type: 'input',
+            message: "Please enter the team manager's full name.",
+            name: 'employeeName',
+            validate: (answer) => {
+                valid = /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/.test(answer)
+                if (!valid) {
+                    return console.log(" *Name input cannot contain any numbers or symbols. Try again.*")
                 }
-            },
-            {
-                type: 'input',
-                message: 'Please enter the team manager\'s id number.',
-                name: 'id',
-                validate: (answer) => {
-                    valid = /^[0-9]+$/.test(answer)
-                    if (!valid) {
-                        return console.log(" *Input must be a number. Try again.*")
-                    }
-                    return true;
-                }
-            },
-            {
-                type: 'input',
-                message: 'Please enter the team manager\'s email address.',
-                name: 'email',
-                default: () => {},
-                validate: function (email) {
-                    valid = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email)
-    
-                    if (valid) {
-                        return true;
-                    } else {
-                       return console.log(" *Not a valid email address format. Try again.*");
-                    }
-                }
-            },
-            {
-                type: 'input',
-                message: "Please enter the team manager\'s office number.",
-                name: 'officeNum',
-                validate: (answer) => {
-                    if (isNaN(answer)) {
-                        return console.log(" *Input must be a number. Try again.*");
-                    }
-                     return true;
-                }                
+                return true;
             }
-        ]).then((response) => {
-        
-            // Stores response variables
-            employeeName = response.employeeName;
-            id = response.id;
-            email = response.email;
-            role = response.role;
-            officeNum = response.officeNum;
+        },
+        {
+            type: 'input',
+            message: 'Please enter the team manager\'s id number.',
+            name: 'id',
+            validate: (answer) => {
+                valid = /^[0-9]+$/.test(answer)
+                if (!valid) {
+                    return console.log(" *Input must be a number. Try again.*")
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: 'Please enter the team manager\'s email address.',
+            name: 'email',
+            default: () => { },
+            validate: function (email) {
+                valid = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email)
 
-            // Once finished, it creates a new Manager from the Manager class with responses as parameters
-            const manager = new Manager(employeeName, id, email, officeNum);
+                if (valid) {
+                    return true;
+                } else {
+                    return console.log(" *Not a valid email address format. Try again.*");
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "Please enter the team manager\'s office number.",
+            name: 'officeNum',
+            validate: (answer) => {
+                if (isNaN(answer)) {
+                    return console.log(" *Input must be a number. Try again.*");
+                }
+                return true;
+            }
+        }
+    ]).then((response) => {
 
-            // Places the manager responses inside of the manager.html file
-            employeeTemplate = fs.readFileSync("./templates/manager.html");
+        // Stores response variables
+        employeeName = response.employeeName;
+        id = response.id;
+        email = response.email;
+        role = response.role;
+        officeNum = response.officeNum;
 
-            // eval accesses the employeeTemplate's local variables and fills them in with responses.
-            teamProfileTemplate +="\n" + eval("`"+ employeeTemplate +"`");
-            console.log("Manager profile successfully created.")
+        // Once finished, it creates a new Manager from the Manager class with responses as parameters
+        const manager = new Manager(employeeName, id, email, officeNum);
 
-            // call the menu prompt to check if user wants to add more employees
-            return menuPrompt();
-        }).catch(err => console.log(err));
-        
+        // Places the manager responses inside of the manager.html file
+        employeeTemplate = fs.readFileSync("./templates/manager.html");
+
+        // eval accesses the employeeTemplate's local variables and fills them in with responses.
+        teamProfileTemplate += "\n" + eval("`" + employeeTemplate + "`");
+        console.log("Manager profile successfully created.")
+
+        // call the menu prompt to check if user wants to add more employees
+        return menuPrompt();
+    }).catch(err => console.log(err));
+
     // Prompt for the menu that will appear after completing each team member prompt
     async function menuPrompt() {
         await inquirer.prompt([
@@ -110,10 +110,10 @@ async function init() {
                 type: 'list',
                 message: 'Would you like to add an additional employee to your team profile?',
                 name: 'role',
-                choices: ["Engineer", "Intern", "Finished Building Team"]    
+                choices: ["Engineer", "Intern", "Finished Building Team"]
             }
         ]).then((menuAnswer) => {
-            
+
             if (menuAnswer.role === "Engineer") {
                 console.log("Creating Engineer Profile...")
                 // Returns the engineer prompt questions
@@ -125,7 +125,7 @@ async function init() {
             } else {
                 return;
             }
-    
+
         }).catch(err => console.error(err));
     }
 
@@ -160,7 +160,7 @@ async function init() {
                 type: 'input',
                 message: 'What is your employee\'s email address?',
                 name: 'email',
-                default: () => {},
+                default: () => { },
                 validate: function (email) {
                     valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
 
@@ -190,7 +190,7 @@ async function init() {
             // Places the engineer responses inside of the engineer.html file
             employeeTemplate = fs.readFileSync("./templates/engineer.html");
 
-            teamProfileTemplate += "\n" + eval("`"+ employeeTemplate +"`")
+            teamProfileTemplate += "\n" + eval("`" + employeeTemplate + "`")
             console.log("Engineer profile successfully created.")
 
             return menuPrompt();
@@ -227,7 +227,7 @@ async function init() {
                 type: 'input',
                 message: 'What is your employee\'s email address?',
                 name: 'email',
-                default: () => {},
+                default: () => { },
                 validate: function (email) {
                     valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
 
@@ -238,14 +238,14 @@ async function init() {
                         return false;
                     }
                 },
-                
+
             },
             {
                 type: 'input',
                 message: 'What school does your intern attend?',
                 name: 'school'
             }
-        // response handler for Intern class and html
+            // response handler for Intern class and html
         ]).then((response) => {
 
             employeeName = response.employeeName;
@@ -260,7 +260,7 @@ async function init() {
             // Places the intern responses inside of the intern.html file
             employeeTemplate = fs.readFileSync("./src/intern.html");
 
-            teamProfileTemplate += eval("`"+ employeeTemplate +"`")
+            teamProfileTemplate += eval("`" + employeeTemplate + "`")
             console.log("Intern profile successfully created.")
 
             // Recall the menu prompt to see if user wants to add another team member
@@ -270,12 +270,12 @@ async function init() {
 
     // Reads generate.html and places the contents of the file in a variable
     const generateFinalPage = fs.readFileSync("./templates/generate.html");
-                        
+
     // takes all of the team profiles that were created, and places them inside of generateFinalPage variable.
-    teamProfileTemplate = eval("`"+ generateFinalPage +"`");
+    teamProfileTemplate = eval("`" + generateFinalPage + "`");
 
     // Writes all of the stored team profiles templates and their contents to a final html page.
-    writeFileAsync("./dist/index.html", teamProfileTemplate);
+    writeFileAsync("./html/index.html", teamProfileTemplate);
     console.log("Team profile page successfully created!")
 }
 
