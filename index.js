b2075b5c993e17bceeff1dcd68758ac49dce01f8
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-05-20 15:58:07 -0400

    created new template

--- index.js
+++ index.js
@@ -1,99 +1,16 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
+// const util = require("util");
+// const writeFileAsync = util.promisify(fs.writeFile);
 
+// imported classes
 const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
-const employee = [];
-let html = ''
-let openhtml = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="style.css">
-    <title>Employee Management Portal</title>
-   
-</head>
-<style>
-.row {
-display: flex;
-justify-content: center;
-justify-content: space-evenly;
-margin: 100px;
-}
-.card-header {
-background-color: rgb(6, 73, 161);
-}
-</style>
-<div class="container-fluid">
-    <div class="card text-dark bg-light mb-3" style="max-width: 18rem;" text-align="center">
-        <div class="jumbotron">My Team</div>
-        <div class="row">
-        </div>
-<div class="row">
-<div class="card text-dark bg-light mb-3" style="max-width: 18rem;" text-align="center">
-    <div class="card-header"></div>
-    <div class="card-header"></div>
-    <div class="card-body">
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-    </div>
-</div>
-<div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-    <div class="card-header"></div>
-    <div class="card-header"></div>
-    <div class="card-body">
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-    </div>
-</div>
-<div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-    <div class="card-header"></div>
-    <div class="card-header"></div>
-    <div class="card-body">
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-    </div>
-</div>
-</div>
-<div class="row">
-<div class="card text-dark bg-light mb-3" style="max-width: 18rem;" text-align="center">
-    <div class="card-header"></div>
-    <div class="card-header"></div>
-    <div class="card-body">
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-    </div>
-</div>
-<div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-    <div class="card-header"></div>
-    <div class="card-header"></div>
-    <div class="card-body">
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-        <script src="code.js"></script>
-<body>`
+let employeeArray = [];
 
-let closehtml = `
-<h3> This completes my team with the members needed. </h3>
-</div>
-</div>
-</body>
-</html>`
 
 // Creates a function to write README file
 function writeToFile(filename) {
